9a4780594cee2350b5608c6e963cec14837ca257
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-01 10:00:00 +0530

    feat: basic multithreading - sequential hello and bye methods

---
+++ Multithreading.java
@@ -0,0 +1,25 @@
+public class Multithreading {
+
+    public static void hello() throws Exception {
+        while (true) {
+            System.out.println("Hello");
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void bye() throws Exception {
+        while (true) {
+            System.out.println("Bye");
+            Thread.sleep(2000);
+        }
+    }
+
+    public static void multithreading() throws Exception {
+        hello();
+        bye();
+    }
+
+    public static void main(String[] args) throws Exception {
+        multithreading();
+    }
+}
