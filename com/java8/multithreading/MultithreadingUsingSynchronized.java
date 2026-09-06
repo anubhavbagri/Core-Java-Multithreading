6ac0d7fc7d9b5571c20376e30d0007e39150b7a1
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-06 11:16:26 +0530

    refactor: replace Thread subclasses with lambdas, encapsulate flag with getter/setter

--- com/java8/multithreading/MultithreadingUsingSynchronized.java
+++ com/java8/multithreading/MultithreadingUsingSynchronized.java
@@ -1,43 +1,52 @@
 package com.java8.multithreading;
 
 public class MultithreadingUsingSynchronized {
-    public static boolean flag = true;
-    public static int limit = 10;
+    boolean flag = true;
+    int limit = 10;
 
-    static class Hello extends Thread {
-        public void run() {
-            for (int i = 1; i <= limit; i += 2) synchronized (MultithreadingUsingSynchronized.class) {
-                try {
-                    while (!flag) MultithreadingUsingSynchronized.class.wait();
-                    // System.out.println("Hello");
-                    System.out.println(i + " odd");
-                    flag = false;
-                    MultithreadingUsingSynchronized.class.notify();
-                } catch (Exception e) {}
-            }
-        }
+    boolean getFlag() {
+        return this.flag;
     }
 
-    static class Bye extends Thread {
-        public void run() {
-            for (int i = 2; i <= limit; i += 2) synchronized (MultithreadingUsingSynchronized.class) {
-                try {
-                    while (flag) MultithreadingUsingSynchronized.class.wait();
-                    // System.out.println("Bye");
-                    System.out.println(i + " even");
-                    flag = true;
-                    MultithreadingUsingSynchronized.class.notify();
-                } catch (Exception e) {}
-            }
-        }
+    void setFlag(boolean flag) {
+        this.flag = flag;
     }
 
-    public static void multithreading() {
-        new Hello().start();
-        new Bye().start();
+    public void multithreading() {
+
+        Thread th1 = new Thread(() -> {
+            for (int i = 1; i <= limit; i += 2)
+                synchronized (MultithreadingUsingSynchronized.class) {
+                    try {
+                        while (!getFlag())
+                            MultithreadingUsingSynchronized.class.wait();
+                        System.out.println(i + " odd");
+                        setFlag(false);
+                        MultithreadingUsingSynchronized.class.notify();
+                    } catch (Exception e) {
+                    }
+                }
+        });
+
+        Thread th2 = new Thread(() -> {
+            for (int i = 2; i <= limit; i += 2)
+                synchronized (MultithreadingUsingSynchronized.class) {
+                    try {
+                        while (getFlag())
+                            MultithreadingUsingSynchronized.class.wait();
+                        System.out.println(i + " even");
+                        setFlag(true);
+                        MultithreadingUsingSynchronized.class.notify();
+                    } catch (Exception e) {
+                    }
+                }
+        });
+
+        th1.start();
+        th2.start();
     }
 
     public static void main(String[] args) {
-        multithreading();
+        new MultithreadingUsingSynchronized().multithreading();
     }
 }
