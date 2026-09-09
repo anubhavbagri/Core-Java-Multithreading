472b0c691cf4823b27c94cfd91d3bc8df486eedc
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-09 18:07:44 +0530

    implement Runnable interface to alternatively print both

--- j_multithreading/Multithreading.java
+++ j_multithreading/Multithreading.java
@@ -1,6 +1,6 @@
 package j_multithreading;
 
-class Hello extends Thread {
+class Hello implements Runnable {
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName());
@@ -14,14 +14,14 @@ class Hello extends Thread {
     }
 }
 
-class Bye extends Thread {
+class Bye implements Runnable {
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName());
         try {
             while (true) {
                 System.out.println("Bye");
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             }
         } catch (Exception e) {
         }
@@ -31,7 +31,18 @@ class Bye extends Thread {
 public class Multithreading {
 
     public static void multithreading() {
-        new Hello().start();
-        new Bye().start();
+        // create an instance of class that implement 'Runnable'
+        Hello hello = new Hello();
+
+        // pass the runnable obj to the thread constructor
+        Thread th1 = new Thread(hello);
+
+        // start the thread
+        th1.start();
+
+        Bye bye = new Bye();
+        Thread th2 = new Thread(bye);
+
+        th2.start();
     }
 }
