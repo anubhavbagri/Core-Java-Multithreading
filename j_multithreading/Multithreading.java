a9c3e51d522d8bb3f84a5fbe7b8aaa73a95c33e5
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-09 22:59:59 +0530

    odd even display using two threads with static inner class implementing Runnable

--- j_multithreading/Multithreading.java
+++ j_multithreading/Multithreading.java
@@ -1,22 +1,23 @@
 package j_multithreading;
 
-// Here the output is synchronized i.e. alternatively displaying hello then bye
+// Here the output is synchronized i.e. alternatively displaying odd then even with static inner class
 // synchronized makes sure that thread cannot run both the blocks simultaneously, only one at a time
 // boolean flag controls the alternate behaviour
 // lock - unlock - signalling
 
 public class Multithreading {
     public static boolean flag = true;
+    public static int limit = 10;
 
-    static class Hello implements Runnable {
+    static class Odd implements Runnable {
         @Override
         public void run() {
-            while (true)
+            for (int i = 1; i <= limit; i += 2)
                 synchronized (Multithreading.class) {
                     try {
                         while (!flag)
                             Multithreading.class.wait();
-                        System.out.println("Hello");
+                        System.out.println(i + " Odd");
                         flag = false;
                         Multithreading.class.notify();
                     } catch (Exception e) {
@@ -25,15 +26,15 @@ public class Multithreading {
         }
     }
 
-    static class Bye implements Runnable {
+    static class Even implements Runnable {
         @Override
         public void run() {
-            while (true)
+            for (int i = 2; i <= limit; i += 2)
                 synchronized (Multithreading.class) {
                     try {
                         while (flag)
                             Multithreading.class.wait();
-                        System.out.println("Bye");
+                        System.out.println(i + " Even");
                         flag = true;
                         Multithreading.class.notify();
                     } catch (Exception e) {
@@ -43,12 +44,12 @@ public class Multithreading {
     }
 
     public static void multithreading() {
-        Hello hello = new Hello();
-        Thread th1 = new Thread(hello);
+        Odd odd = new Odd();
+        Thread th1 = new Thread(odd);
         th1.start();
 
-        Bye bye = new Bye();
-        Thread th2 = new Thread(bye);
+        Even even = new Even();
+        Thread th2 = new Thread(even);
         th2.start();
     }
 }
