97ce14688dd44fee948624607421ad1eee8f0f72
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-09 17:47:39 +0530

    via Main Thread

--- j_multithreading/Main.java
+++ j_multithreading/Main.java
@@ -1,7 +1,8 @@
 package j_multithreading;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Thread name: " + Thread.currentThread().getName());
         Multithreading.multithreading();
     }
 }
