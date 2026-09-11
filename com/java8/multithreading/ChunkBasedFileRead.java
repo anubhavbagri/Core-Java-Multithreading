6943d10366002c20f6cc9c0da38fb4bda072c232
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-11 22:21:00 +0530

    creates a sample file of specified size

---
+++ com/java8/multithreading/ChunkBasedFileRead.java
@@ -0,0 +1,33 @@
+package com.java8.multithreading;
+
+// import java.io.BufferedOutputStream;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+
+
+public class ChunkBasedFileRead {
+
+    // public static void generateLargeFile(String filename, long sizeInMB) {
+    //     try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename))) {
+
+    //         byte[] data = "The quick brown fox jumped over the lazy dog ".getBytes();
+
+    //         long targetBytes = sizeInMB * 1024 * 1024;
+    //         long writtenBytes = 0;
+
+    //         while (writtenBytes < targetBytes) {
+    //             out.write(data);
+    //             writtenBytes += data.length;
+    //         }
+    //     } catch (IOException e) {
+
+    //     }
+    // }
+
+    public static void main(String[] args) {
+
+
+        // generateLargeFile("sample.txt", 1);
+
+    }
+}
