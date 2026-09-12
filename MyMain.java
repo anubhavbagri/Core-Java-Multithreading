e965c3d2f0728533b59ab6ce6f317a88afc9b1b5
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-12 14:09:10 +0530

    fix error

---
+++ MyMain.java
@@ -0,0 +1,38 @@
+// Create an EmailNotification class that extends an abstract Message class and implements a Notification interface,
+// then demonstrates inheritance, interface implementation, and polymorphism in MyMain.java.
+
+interface Notification {
+    void getChannel();
+}
+
+abstract class Message {
+    String getMessage(int id) {
+        return "order " + id + " sent";
+    }
+}
+
+class EmailNotification extends Message implements Notification {
+
+    public void getChannel() {
+        System.out.println("Notify via email");
+    }
+}
+
+class SlackNotification extends Message implements Notification {
+
+    public void getChannel() {
+        System.out.println("Notify via slack");
+    }
+}
+
+public class MyMain {
+    public static void main(String[] args) {
+        EmailNotification emailObj = new EmailNotification();
+        emailObj.getChannel();
+        System.out.println(emailObj.getMessage(505));
+
+        SlackNotification slackObj = new SlackNotification();
+        slackObj.getChannel();
+        System.out.println(slackObj.getMessage(506));
+    }
+}
