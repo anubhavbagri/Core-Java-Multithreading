5710a92eec739f31c3d12bf8e2b3ef6c9c6d9862
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-20 10:49:25 +0530

    streamAPI intro

---
+++ k_streamAPI/Main.java
@@ -0,0 +1,10 @@
+package k_streamAPI;
+
+// Stream API is used to process collections of objects in a functional-style and declarative way
+// stream in java is a sequence of objects that supports various methods which
+// can be pipelined to produce the desired result
+// no storage, only pipeline of functions
+
+public class Main {
+
+}
