a1a284691225fdc6c2e7b4d8a24d9cbc5beb8196
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-21 22:54:01 +0530

    print prime numbers upto n using IntStream

--- com/java8/streams/MyMain.java
+++ com/java8/streams/MyMain.java
@@ -2,8 +2,8 @@ package com.java8.streams;
 
 import java.util.*;
 import java.util.function.*;
-
-import javax.sql.rowset.spi.SyncResolver;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 // 1. find only odd numbers from a stream
 class A {
@@ -24,14 +24,17 @@ class B {
     public static void call() {
         List<Integer> salaries = List.of(50000, 30000, 25000, 60000, 25000);
 
-        // int[] count = { 0 };
+        int[] count = { 0 };
 
-        // salaries.stream()
-        // .distinct()
-        // .sorted((a, b) -> b - a)
-        // .filter(n -> ++count[0] == 3)
-        // .forEach(System.out::print);
+        // not readable but time efficient
+        salaries.stream()
+                .distinct()
+                .sorted((a, b) -> b - a)
+                .filter(n -> ++count[0] == 2)
+                .forEach(System.out::println);
 
+        // readable but not time efficient since for each item of the stream, new object
+        // is being created
         salaries.stream()
                 .distinct()
                 .sorted(Comparator.reverseOrder())
@@ -45,12 +48,47 @@ class B {
                         }
                     }
                 });
+
+        salaries.stream()
+                .distinct()
+                .sorted((a, b) -> b - a)
+                .skip(1)
+                .findFirst()
+                .ifPresent(System.out::print);
     }
 }
+
+// 3. find sum of digits of a given number using stream api
 
 class C {
     public static void call() {
     }
+}
+
+// 4. print only prime numbers from 1 to n using stream api
+class D {
+    public static void call() {
+        int n = 10;
+
+        Supplier<Integer> s = new Supplier<Integer>() {
+            int i = 1;
+
+            @Override
+            public Integer get() {
+                return i++;
+            }
+        };
+
+        Predicate<Integer> isPrime = (num) -> num > 1 && IntStream.range(2, num)
+                .noneMatch(i -> num % i == 0);
+
+        Stream.generate(s)
+                .limit(n)
+                .filter(isPrime)
+                .forEach(System.out::println);
+
+    }
+
 }
 
 public class MyMain {
@@ -60,5 +98,7 @@ public class MyMain {
         B.call();
         System.out.println();
         C.call();
+        System.out.println("Prime numbers: ");
+        D.call();
     }
 }
